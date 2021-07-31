124eb439b24fdfd137013a3cf2ebd5cef5075cb6
xing.hu <xing.hu@mobilemd.cn>
2021-07-31 23:06:26 +0800

    auto docs

--- .umirc.ts
+++ .umirc.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'umi'
-console.log('环境变量', process.env.NODE_ENV);
 // dumi 文档配置
 export default defineConfig({
     mode: 'site',
     title: 'mk站点文档',
-    // publicPath: process.env.NODE_ENV !== 'production' ? '/mk-packages/' : '/mk-packages/',
-    publicPath: './',
+    // publicPath: './',
+    publicPath: '/mk-packages/',
+    base: '/mk-packages/',
     outputPath: 'docs',
-    history: {
-        type: 'hash'
-    },
+    // history: {
+    //     type: 'hash'
+    // },
     resolve: {
         includes: [
             'site-docs',
--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 // 更新package.json版本号
 const path = require('path');
+const pkg = require('./package.json');
+
+const packages = [
+    'packages/react-eslint',
+    'packages/react-hooks',
+    'packages/react-ui',
+    'packages/vue-eslint',
+];
+
