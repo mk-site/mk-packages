281bc68773da5ed6be8c9366995d7e53af9c40c9
xing.hu <xing.hu@mobilemd.cn>
2021-07-31 22:46:07 +0800

    feat: 静态站点

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'umi'
-
+console.log('环境变量', process.env.NODE_ENV);
 // dumi 文档配置
 export default defineConfig({
     mode: 'site',
     title: 'mk站点文档',
+    publicPath: process.env.NODE_ENV !== 'production' ? '/' : '/mk-packages/',
     outputPath: 'docs',
     // history: {
     //     type: 'hash'
